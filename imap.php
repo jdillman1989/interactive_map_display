f1a4a756114739f5e3693231dda9732f3e424266
Jesse Dillman <JDillman@bibleleague.org>
2014-10-15 09:28:41 -0500

    Initial upload

---
+++ imap.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html>
+  <head>
+
+    <meta charset="utf-8">
+
+    <!-- Always force latest IE rendering engine or request Chrome Frame -->
+    <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
+
+    <!-- Use title if it's in the page YAML frontmatter -->
+    <title>Bible League International | Where We Serve</title>
+
+    <link href="css/reset.css" rel="stylesheet" type="text/css" />
+
+    <link href="css/jqvmap.css" rel="stylesheet" type="text/css" />
+
+    <script type="text/javascript" src="js/jquery-1.7.2.min.js"></script>
+
+    <script type="text/javascript" src="js/jquery.vmap.js"></script>
+
+    <script type="text/javascript" src="js/jquery.vmap.world.js"></script>
+
+    <script type="text/javascript" src="js/jquery.vmap.un_regions.js"></script>
+
+    <script type="text/javascript" src="js/region_colors.js"></script>
+    
+    <script type="text/javascript" src="js/ChartNew.js"></script>
+
+  </head>
+
+  <body>
+
+    <div class="map">
+        <div id="vmap"></div>
+
+        <div id="canvas-holder">
+            <div id='worldwide'><p>Worldwide stats</p></div>
+
+            <canvas id="bar" height="190px"></canvas>
+
+            <div id="loader"></div>
+
+        </div>
+
+        <div id="post"></div>
+
+    </div>
+
+    <script type="text/javascript" src="js/start.js"></script>
+  </body>
+
+</html>
